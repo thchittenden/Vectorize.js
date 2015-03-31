57a3eab8aa8b7147886a3ffff65432419db44000
Tom Chittenden <thchittenden@cmu.edu>
2015-03-31 19:57:56 -0400

    Updated Tests and moved testing page.

--- tests/basic.js
+++ tests/basic.js
@@ -1,9 +1,35 @@
-QUnit.test("basic test", function(assert) {
-    start = performance.now();
-    var sum = 0;
-    for (var i = 0; i < 1000; i++) {
-        sum += i;
+tests.push({
+    name: 'Vector Add Literal',
+    args: [0, 1, 2, 3, 4, 5, 6, 7],
+    fn: function fn (args) {
+        for (var i = 0; i < args.length; i++) {
+            args[i] = args[i] + 1;
+        }
+        return args;
     }
-    end = performance.now();
-    assert.ok(sum == sum, "Passed! " + ((end - start)/1000) + " ms");
+});
+
+tests.push({
+    name: 'Vector Add Identifier',
+    args: [0, 1, 2, 3, 4, 5, 6, 7],
+    fn: function fn (args) {
+        var x = 2;
+        for (var i = 0; i < args.length; i++) {
+            args[i] = args[i] + x;
+        }
+        return args;
+    }
+});
+
+tests.push({
+    name: 'Vector Add Vector',
+    args: { arg1: [0, 1, 2, 3], arg2: [4, 5, 6, 7] },
+    fn: function fn (args) {
+        var a = args.arg1;
+        var b = args.arg2;
+        var c = [];
+        for (var i = 0; i < a.length; i++) {
+             c[i] = a[i] + b[i];
+        }
+    }
 });
